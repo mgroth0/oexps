7bb1be90b2a5d58efdb0118abca5a590004aa82a
mgroth0 <mgroth49@gmail.com>
2023-06-12 11:30:11 -0400

    changed 2 files

--- src/main/python/BRS1.py
+++ src/main/python/BRS1.py
@@ -1,23 +1,25 @@
-import sys
+import json
 from pathlib import Path
 import oexp
-from oexp import access
-import os
+import argparse
+
+parser = argparse.ArgumentParser(description='Open or print experiment URL')
+# parser.add_argument('url', type=str, help='the experiment URL')
+parser.add_argument('-o', '--open', action='store_true', help='open the experiment in Chrome')
+parser.add_argument('-p', '--print', action='store_true', help='print the experiment URL')
+
+args = parser.parse_args()
 
 this_file = Path(__file__)
 
 STYLE_FILE = this_file.parent.joinpath("BRS1.css")
 
-matt_user = os.environ['MATT_USER']
-matt_pw = os.environ['MATT_PW']
-if (ktor_port := os.getenv("MATT_KTOR_PORT")) is not None:
-    # noinspection PyProtectedMember
-    # noinspection PyArgumentList
-    access.API.enable_local_mode(int(ktor_port))
+with open(this_file.parent.joinpath(".auth.json")) as f:
+    auth_json = json.loads(f.read())
 
-user = oexp.login(matt_user, matt_pw)
+user = oexp.login(auth_json["username"], auth_json["password"])
 
-exp = user.experiment("chrometest")
+exp = user.experiment("BRS1")
 exp.delete_all_images()
 ims = exp.upload_images("/Users/matthewgroth/registered/bolt/oexp_demo/images")
 uploaded_images = exp.list_images()
@@ -36,3 +38,19 @@ for m in range(20):
 exp.manifests = manifests
 with open(STYLE_FILE, "r") as f:
     exp.css = f.read()
+
+
+if args.print:
+    print("Experiment URL:", exp.session_url())
+
+if args.open:
+    exp.open()
+
+
+args = parser.parse_args()
+
+
+
+
+
+
